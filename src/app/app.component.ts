72b1e16263705e58acbd92fafbf3ef73ca28cb2a
duvanceliz <duvanceliz@gmail.com>
2022-03-04 18:35:02 -0500

    my briefcase con bonton hamburguesa integrado y avance del 70%

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myBriefCase_v2';
+  i:number = 0;
+
+  
+  onClick(){
+
+    this.i+=1;
+
+    const nav = document.querySelector("#hamburger button");
+    const navbar = document.querySelector("nav");
+
+    if(this.i == 1){
+
+      nav?.classList.add('open');
+      nav?.classList.remove('close');
+
+      navbar?.classList.add('open')
+      navbar?.classList.remove('close')
+
+    }else if(this.i == 2){
+      nav?.classList.remove('open')
+      nav?.classList.add('close');
+
+      navbar?.classList.add('close')
+      navbar?.classList.remove('open')
+      this.i=0;
+    }
+    
+    
+
+  }
 }
