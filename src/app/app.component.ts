951c88640f5f4329345b1b7bbe6657ee07f1971f
duvanceliz <duvanceliz@gmail.com>
2022-03-06 11:47:13 -0500

    my briefcase con animaciones en imagenes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    // animation triggers go here
+  ]
 })
-export class AppComponent {
-  title = 'myBriefCase_v2';
-  i:number = 0;
+export class AppComponent implements OnInit{
+  constructor(private renderer2: Renderer2){
+
+
+  }
+ 
+  title:string='';
+
+  i:boolean = true;
+
+  @ViewChild('btnHamburger')
+  btnHamburger!: ElementRef;
+
+  @ViewChild('navbar')
+  nav!: ElementRef;
+
+  @ViewChild('imgOne')
+  imgOne!: ElementRef;
+
+  @ViewChild('imgTwo')
+  imgTwo!: ElementRef;
+  
+  @ViewChild('imgThree')
+  imgThree!: ElementRef;
+
+
+  @HostListener('window:scroll',[]) 
+  onScroll():void {
+        const imgOne = this.imgOne.nativeElement;
+        const imgTwo = this.imgTwo.nativeElement;
+        const imgThree = this.imgThree.nativeElement;
+
+      let windowScrollPos = window.scrollY;
+       
+       if(windowScrollPos >imgOne.offsetTop/3){
+
+        this.renderer2.addClass(imgOne,'mover-right');
+
+       }
+
+       if(windowScrollPos > imgTwo.offsetTop/1.5){
+        this.renderer2.addClass(imgTwo,'mover-left');
+
+       }
+       if(windowScrollPos > imgThree.offsetTop/1.5){
+        this.renderer2.addClass(imgThree,'mover-right');
+
+       }
+        
+  };
 
   
+  ngOnInit(): void {
+    
+   
+  }
+ 
   onClick(){
+  
+    const btnH = this.btnHamburger.nativeElement;
+    const navB = this.nav.nativeElement;
+    
+    if(this.i){
 
-    this.i+=1;
+      this.renderer2.setAttribute(btnH,'class','open')
+      this.renderer2.setAttribute(navB,'class','open')
+   
+      this.i = !this.i;
 
-    const nav = document.querySelector("#hamburger button");
-    const navbar = document.querySelector("nav");
-
-    if(this.i == 1){
-
-      nav?.classList.add('open');
-      nav?.classList.remove('close');
-
-      navbar?.classList.add('open')
-      navbar?.classList.remove('close')
-
-    }else if(this.i == 2){
-      nav?.classList.remove('open')
-      nav?.classList.add('close');
-
-      navbar?.classList.add('close')
-      navbar?.classList.remove('open')
-      this.i=0;
+    }else{
+      this.renderer2.setAttribute(btnH,'class','close')
+      this.renderer2.setAttribute(navB,'class','close')
+  
+      this.i = !this.i;
     }
-    
-    
 
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,7 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,RouterModule
+    BrowserModule,RouterModule,BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
